ee8e8a0945832d55b29503b33c0fc953f1bcdfe7
Alex McQuilkin <alexmcq99@gatech.edu>
2022-02-08 19:07:11 -0500

    Basic functionality now works; need to fix video -> audio files and csv bug

--- app.py
+++ app.py
@@ -1,11 +1,50 @@
 import asyncio
+import csv
 import discord
 from dotenv import load_dotenv
 import os
 import youtube_dl
+from urllib.parse import urlparse, parse_qs
+from contextlib import suppress
+from discord.ext import commands
 
-from discord.ext import commands, tasks
+def read_downloaded_songs(song_file):
+    try:
+        with open(song_file, mode="r") as f:
+            reader = csv.reader(f)
+            next(reader)
+            downloaded_songs = {row[0]: tuple(row[1:]) for row in reader}
+    except Exception as e:
+        print("Error in reading file: " + str(e))
+        write_downloaded_song(song_file, ["id", "title", "file_path"])
+        downloaded_songs = {}
+    return downloaded_songs
 
+def write_downloaded_song(song_file, row):
+    with open(song_file, mode="a") as f:
+        writer = csv.writer(f)
+        writer.writerow(row)
+
+# noinspection PyTypeChecker
+def get_yt_id(url, ignore_playlist=True):
+    # Examples:
+    # - http://youtu.be/SA2iWivDJiE
+    # - http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu
+    # - http://www.youtube.com/embed/SA2iWivDJiE
+    # - http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
+    query = urlparse(url)
+    if query.hostname == 'youtu.be': return query.path[1:]
+    if query.hostname in {'www.youtube.com', 'youtube.com', 'music.youtube.com'}:
+        if not ignore_playlist:
+        # use case: get playlist id not current video in playlist
+            with suppress(KeyError):
+                return parse_qs(query.query)['list'][0]
+        if query.path == '/watch': return parse_qs(query.query)['v'][0]
+        if query.path[:7] == '/watch/': return query.path.split('/')[1]
+        if query.path[:7] == '/embed/': return query.path.split('/')[2]
+        if query.path[:3] == '/v/': return query.path.split('/')[2]
+   # returns None for invalid YouTube url
+    
 # youtube download source object
 class YTDLSource(discord.PCMVolumeTransformer):
     def __init__(self, source, *, data, volume=0.5):
@@ -39,7 +78,7 @@ ytdl_format_options = {
     'noplaylist': True,
     'nocheckcertificate': True,
     'ignoreerrors': False,
-    'logtostderr': False,
+    'logtostderr': True,
     'quiet': False,
     'no_warnings': True,
     'default_search': 'auto',
@@ -57,6 +96,9 @@ ffmpeg_options = {
 intents = discord.Intents().default()
 client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='-',intents=intents)
+
+SONG_FILE = "downloaded_songs.csv"
+downloaded_songs = read_downloaded_songs(SONG_FILE)
 
 # define bot commands
 @bot.command(name='join', help='Joins the voice channel')
@@ -90,29 +132,34 @@ async def play(ctx,url):
             channel = ctx.message.author.voice.channel
             await channel.connect()
     try:
-        vc = ctx.message.guild.voice_client
-        async with ctx.typing():
-            filename = await YTDLSource.from_url(url, loop=bot.loop)
-            print(f"Filename: {filename}")
-            info = ytdl.extract_info(url, download=False)
-            id, title = info['id'], info['title']
-            vc.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
+        if not ctx.message.guild.voice_client.is_playing():
+            async with ctx.typing():
+                id = get_yt_id(url)
+                # Check if we've already downloaded the song, download it now if we haven't
+                if id in downloaded_songs:
+                    title, file_path = downloaded_songs[id]
+                    print("File exists, got info")
+                else:
+                    await ctx.send(f'Please wait...currently downloading music')
+                    data = await asyncio.get_event_loop().run_in_executor(None, lambda: ytdl.extract_info(url, download=True))
+                    if 'entries' in data:
+                        # take first item from a playlist
+                        data = data['entries'][0]
+                    title, local_filename = data['title'], ytdl.prepare_filename(data)
+                    file_path = os.path.join(MUSIC_PATH, local_filename)
+                    os.replace(local_filename, file_path)
+                    downloaded_songs[id] = (title, file_path)
+                    write_downloaded_song(SONG_FILE, [id, title, file_path])
+                    print("File doesn't exist, downloaded it")
 
-            # Get metadata
-            # info = ytdl.extract_info(url, download=False)
-            # title, filename = info['title'], ytdl.prepare_filename(info)
-            # print(f"Filename: {filename}")
-            # Check if file already exists, download if it doesn't
-            # full_path = os.path.join(MUSIC_PATH, filename)
-            # if not os.path.exists(full_path):
-            #     print("File doesn't exist, downloading it")
-            #     ytdl.download([url])
-            #     os.replace(filename, full_path)
-            # Play music    
-            # vc.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=full_path))
-        await ctx.send(f'**Now playing:** {title}')
+                print("Playing audio")
+                ctx.message.guild.voice_client.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=file_path))
+            await ctx.send(f'**Now playing:** {title}')
+        else:
+            await ctx.send("Song queued.")
     except Exception as e:
         print("THIS IS THE ERROR\n" + str(e))
+        print(type(e))
         await ctx.send("An error occurred.  I blame Devin.")
 
 @bot.command(name='pause', help='Pauses the song')
