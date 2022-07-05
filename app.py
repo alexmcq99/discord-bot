1a4ddd9254fa9cd28eedf4a9b1b5c4c4ac8383d5
Alex McQuilkin <alexmcq99@gatech.edu>
2022-07-05 16:44:22 -0400

    Refactored and fixed stats functionality

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from collections import deque
 from contextlib import suppress
 from discord.ext import commands
 import json
-from pytube import YouTube
+from pytube import Search, YouTube
 import random
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -17,33 +17,11 @@ from unidecode import unidecode
 from urllib.parse import urlparse, parse_qs
 from urllib.request import urlopen
 
-# Read downloaded songs file
-# Create the file and return an empty dictionary if it doesn't exist
-def read_downloaded_songs(song_file):
-    try:
-        with open(song_file, mode="r", encoding="utf-8", errors="ignore") as f:
-            reader = csv.reader(f)
-            next(reader)
-            data = {id: (title, path, int(duration), True) for id, title, path, duration in reader}
-    except FileNotFoundError as e:
-        write_downloaded_song(song_file, ["id", "title", "file_path", "duration"])
-        data = {}
-    except Exception as e:
-        print("Unexpected error in reading file: " + str(e))
-
-    return data
-
-# Write a song's metadata to the downloaded songs file after it's downloaded
-def write_downloaded_song(song_file, row):
-    with open(song_file, mode="a", newline="", encoding="utf-8", errors="ignore") as f:
-        writer = csv.writer(f)
-        writer.writerow(row)
-
-# Read a stats file
+# Read a json file to a dictionary
 # Return an empty dictionary if not found
-def read_stats(stats_file):
+def read_json_file(file):
     try:
-        with open(stats_file) as f:
+        with open(file) as f:
             data = json.load(f)
     except FileNotFoundError as e:
         data = {}
@@ -52,42 +30,41 @@ def read_stats(stats_file):
     
     return data
 
-# Write stats back to disk
-# Done when bot exits, stats are stored in dictionaries while running
-def write_stats(stats_file, stats):
-    with open(stats_file, "w") as f:
-        json.dump(stats, f)
+# Write data back to disk as json file
+# Done when bot exits, data is stored in dictionaries while running
+def write_json_file(file, data):
+    with open(file, "w") as f:
+        json.dump(data, f, indent=4)
 
-# get api tokens from .env
+# Get api tokens from .env
 load_dotenv()
 DISCORD_TOKEN = os.getenv("discord_token")
 SPOTIPY_CLIENT_ID = os.getenv("spotipy_client_id")
 SPOTIPY_CLIENT_SECRET = os.getenv("spotipy_client_secret")
 
-# constants
+# Constants
 DURATION_LIMIT = 1200 # Any videos longer than this (20 minutes in seconds) will not be downloaded
 MAX_SHOWN_SONGS = 10 # The maximum number of songs to show when displaying the song queue
+SPOTIFY_SONG_LIMIT = 50 # The maximum number of songs to add from a spotify playlist
 
-# paths
+# Paths
 MUSIC_PATH = "music" # Directory where music files are stored
-SONG_FILE = "downloaded_songs.csv" # File with metadata on downloaded songs
-USER_STATS_FILE = "user_stats.json" # File with stats grouped by user
-SONG_STATS_FILE = "song_stats.json" # File with stats grouped by song
+SONG_FILE = "song_data.json" # File with metadata on downloaded songs
+USER_FILE = "user_data.json" # File with usage data grouped by user
 
 # Create directory if it doesn't exist
 if not os.path.exists(MUSIC_PATH):
     os.makedirs(MUSIC_PATH)
 
 # Read files into dictionaries
-downloaded_songs = read_downloaded_songs(SONG_FILE)
-user_stats = read_stats(USER_STATS_FILE)
-song_stats = read_stats(SONG_STATS_FILE)
+song_data = read_json_file(SONG_FILE)
+user_data = read_json_file(USER_FILE)
 
 # Spotify credentials and object
 creds = SpotifyClientCredentials(SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET)
 spotify = spotipy.Spotify(client_credentials_manager=creds)
 
-# make bot
+# Make bot
 intents = discord.Intents().default()
 client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix='-',intents=intents)
@@ -117,7 +94,7 @@ def get_spotify_info(url):
         result = spotify.track(id)
         search_list = [result['artists'][0]['name'].split() + "-" + result['name'].split()]
     elif type == "playlist":
-        result = spotify.playlist_tracks(id, limit=50)
+        result = spotify.playlist_tracks(id, limit=SPOTIFY_SONG_LIMIT)
         search_list = [item['track']['artists'][0]['name'].split() + ["-"] + item['track']['name'].split() for item in result['items']]
     return search_list
 
@@ -164,20 +141,24 @@ def yt_url_to_id(url, ignore_playlist=True):
         if query.path[:3] == '/v/': return query.path.split('/')[2]
    # returns None for invalid YouTube url
 
-# Retrieves song metadata and stores it in the downloaded_songs dictionary
+# Retrieves song metadata and stores it in the song_datas dictionary
 # Will optionally download the song file itself (since it's costly)
 def get_song(id, download=True):
     yt = None
-    if id in downloaded_songs: # Get metadata if we already have it
-        title, file_path, duration, downloaded = downloaded_songs[id]
+    if id in song_data: # Get metadata if we already have it
+        data = song_data[id]
     else: # Otherwise, retrieve it
         yt = YouTube(yt_id_to_url(id))
-        title, duration = yt.title, yt.length
-        file_path = None
-        downloaded = False
+        data = {
+            "title": yt.title,
+            "duration": yt.length,
+            "downloaded": False,
+            "file path": None,
+            "request count": 0
+        }
 
     # Only download if we want to and haven't already downloaded it
-    if download and not downloaded:
+    if download and not data["downloaded"]:
         if not yt:
             yt = YouTube(yt_id_to_url(id))
         stream = yt.streams.filter(only_audio=True).first()
@@ -185,13 +166,10 @@ def get_song(id, download=True):
         base, _ = os.path.splitext(out_file)
         file_path = os.path.join(MUSIC_PATH, f"{base}.mp3")
         os.rename(out_file, file_path)
-        downloaded = True
 
-        # Write downloaded song metadata to downloaded songs file
-        write_downloaded_song(SONG_FILE, [id, title, file_path, duration])
+        data["file path"], data["downloaded"] = file_path, True
     
-    data = (title, file_path, duration, downloaded)
-    downloaded_songs[id] = data
+    song_data[id] = data
     return data
 
 # Bot commands
@@ -239,7 +217,8 @@ def play_next(ctx):
     # If current song is finished and there's something queued, play it
     if len(song_queue) >= 1 and not ctx.voice_client.is_playing():
         curr_song_id = song_queue.popleft()
-        title, file_path, _, _ = get_song(curr_song_id) # Downloads the song if not already downloaded
+        data = get_song(curr_song_id) # Downloads the song if not already downloaded
+        title, file_path = data["title"], data["file path"]
         asyncio.run_coroutine_threadsafe(ctx.send(f'**Now playing:** :notes: {title} :notes:'), loop=bot.loop)
         ctx.message.guild.voice_client.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=file_path), after=lambda e: play_next(ctx))
     # No current song
@@ -266,10 +245,12 @@ async def play(ctx, *args):
     if yt_id: # We have a youtube url
         songs_to_add.append(yt_id)
     elif is_spotify_url(url): # We have a spotify url
+        print("getting spotify data")
         search_queries.extend(get_spotify_info(url))
     else: # We have a search query
         search_queries.append(args)
     
+    print("doing youtube searches")
     # Do youtube search on search queries to get ids
     for query in search_queries:
         search_result = yt_search(query)
@@ -277,10 +258,12 @@ async def play(ctx, *args):
             await ctx.send("\"" + " ".join(query) + "\" did not yield any search results.")
         songs_to_add.append(search_result)
 
+    print("getting song metadata and adding to queue")
     for song_id in songs_to_add:
         # Get song metadata without downloading
         # We download just before playing the song to avoid downloading one song while playing another
-        title, _, duration, _ = get_song(song_id, download=False)
+        data = get_song(song_id, download=False)
+        title, duration = data["title"], data["duration"]
 
         if duration > DURATION_LIMIT: # Check if song is too long
             duration_str = f"{str(DURATION_LIMIT // 60).zfill(2)}:{str(DURATION_LIMIT % 60).zfill(2)}"
@@ -290,13 +273,13 @@ async def play(ctx, *args):
             await ctx.send(f":notes: {title} :notes: is currently streaming live and cannot be downloaded.")
             continue
 
-        # Update user stats
-        if ctx.author.mention not in user_stats:
-            user_stats[ctx.author.mention] = {}
-        user_stats[ctx.author.mention][song_id] = user_stats[ctx.author.mention].get(song_id, 0) + 1
+        # Update user usage data
+        if ctx.author.mention not in user_data:
+            user_data[ctx.author.mention] = {}
+        user_data[ctx.author.mention][song_id] = user_data[ctx.author.mention].get(song_id, 0) + 1
 
-        # Update song stats
-        song_stats[song_id] = song_stats.get(song_id, 0) + 1
+        # Update request count for song
+        song_data[song_id]["request count"] += 1
 
         # Add to queue and play the next song
         song_queue.append(song_id)
@@ -306,7 +289,7 @@ async def play(ctx, *args):
 @bot.command(name='playall', help='Plays all downloaded songs')
 async def playall(ctx):
     await join(ctx) # join the voice channel
-    song_queue.extend(downloaded_songs.keys()) # Add all downloaded songs to the queue
+    song_queue.extend([id for id, data in song_data.items() if data["downloaded"]]) # Add all downloaded songs to the queue
     await ctx.send("Successfully added all downloaded songs to the queue.")
     play_next(ctx) # play the next song in the queue
 
@@ -321,12 +304,12 @@ async def loop(ctx):
 async def showqueue(ctx):
     global is_looping, curr_song_id
     loop_msg = f"The queue is {'' if is_looping else 'not '}looping.\n" # Display if the queue is looping
-    curr_song_title = downloaded_songs[curr_song_id][0] if curr_song_id else 'None'
+    curr_song_title = song_data[curr_song_id]["title"] if curr_song_id else 'None'
     curr_song_msg = f"Current song: {curr_song_title}\n" # Display the current song
     queue_count_msg = f"Number of songs in queue: {len(song_queue)}\n" # Display the length of the queue
     if len(song_queue) > 0: # Display only the first few songs
         queue_header = "Songs in queue:\n" if len(song_queue) <= MAX_SHOWN_SONGS else f"First {MAX_SHOWN_SONGS} songs:\n"
-        queue_contents = "\n".join([f"{i}) {downloaded_songs[id][0]}" for i, id in enumerate(list(song_queue)[:MAX_SHOWN_SONGS], 1)])
+        queue_contents = "\n".join([f"{i}) {song_data[id]['title']}" for i, id in enumerate(list(song_queue)[:MAX_SHOWN_SONGS], 1)])
     else:
         queue_header, queue_contents = "", ""
     msg = loop_msg + curr_song_msg + queue_count_msg + queue_header + queue_contents
@@ -336,7 +319,7 @@ async def showqueue(ctx):
 async def remove(ctx, position):
     position = int(position)
     if position > 0 and position <= len(song_queue):
-        title = downloaded_songs[song_queue[position - 1]][0]
+        title = song_data[song_queue[position - 1]]["title"]
         del song_queue[position - 1]
         await ctx.send(f":notes: {title} :notes: at position {position} has been removed from the queue.")
     else:
@@ -350,7 +333,7 @@ async def skip(ctx):
     elif not ctx.voice_client.is_playing():
         await ctx.send("No song is currently playing.")
     else:
-        curr_song_title = downloaded_songs[curr_song_id][0]
+        curr_song_title = song_data[curr_song_id]["title"]
         curr_song_id = None
         ctx.voice_client.stop()
         await ctx.send(f"Skipped :notes: {curr_song_title} :notes:")
@@ -380,13 +363,14 @@ async def resume(ctx):
 
 @bot.command(name='stop', help='Stops the song and clears the queue')
 async def stop(ctx):
-    global curr_song_id
+    global curr_song_id, is_looping
     if not ctx.voice_client:
         await ctx.send("The bot is not currently in a voice channel.")
     elif not ctx.voice_client.is_playing():
         await ctx.send("No song is currently playing.")
     else:
         curr_song_id = None
+        is_looping = False
         song_queue.clear()
         ctx.message.guild.voice_client.stop()
         await ctx.send("Stopped current song and cleared the song queue.")
@@ -404,37 +388,83 @@ def calculate_global_stats():
     global_stats = {}
 
     # Total requests
-    global_stats["Total requests"] = sum(song_stats.values())
+    global_stats["Total requests"] = sum([data["request count"] for data in song_data.values()])
 
     # Total duration
-    total_duration = sum([downloaded_songs[id][2] * song_stats[id] for id in song_stats])
+    total_duration = sum([data["request count"] * data["duration"] for data in song_data.values()])
     hours = total_duration // 3600
     mins = (total_duration % 3600) // 60
     seconds = (total_duration % 3600) % 60
     global_stats["Total duration of requested music"] = f"{str(hours).zfill(2)}:{str(mins).zfill(2)}:{str(seconds).zfill(2)}"
 
     # Most requested song
-    if 
-    most_popular_id = max(song_stats, key= lambda song: song_stats[song])
-    most_popular_title = downloaded_songs[most_popular_id][0] if len(song_stats) > 0 else "N/A"
-    num_plays = song_stats[most_popular_id] if len(song_stats) > 0 else 0
-    global_stats["Most requested song"] = f"{most_popular} with {num_plays} requests"
-    user_requests = {user: sum(user_stats[user].values()) for user in user_stats}
-    most_active = max(user_requests, key= lambda user: user_requests[user]) if len(user_requests) > 0 else "N/A"
-    num_requests = user_requests[most_active] if len(user_requests) > 0 else 0
-    global_stats["Most active user"] = f"{most_active} with {num_requests} requests"
+    if len(song_data) > 0:
+        most_popular_id = max(song_data, key= lambda id: song_data[id]["request count"])
+        most_popular_title = song_data[most_popular_id]["title"]
+        request_count = song_data[most_popular_id]["request count"]
+        msg = f"{most_popular_title} with {request_count} requests"
+    else:
+        msg = "N/A"
+    global_stats["Most requested song"] = msg
+
+    # Most active user
+    user_requests = {user: sum(user_data[user].values()) for user in user_data}
+    if len(user_requests) > 0:
+        most_active = max(user_requests, key= lambda user: user_requests[user])
+        request_count = user_requests[most_active]
+        msg = f"{most_active} with {request_count} requests"
+    else:
+        msg = "N/A"
+    global_stats["Most active user"] = msg
+
     return global_stats
 
-@bot.command(name='stats', help='Retrieves usage statistics for the bot and its users')
-async def stats(ctx):
-    global_stats = calculate_global_stats()
-    msg = "Global statistics:\n" + "\n".join([f"{name}: {value}" for name, value in global_stats.items()])
+# Calculates user stats based off of the user stats dictionary
+def calculate_user_stats(user):
+    data = user_data[user]
+    user_stats = {}
+
+    # Total requests
+    user_stats["Total requests"] = sum(data.values())
+
+    # Total duration
+    total_duration = sum([data[id] * song_data[id]["duration"] for id in data])
+    hours = total_duration // 3600
+    mins = (total_duration % 3600) // 60
+    seconds = (total_duration % 3600) % 60
+    user_stats["Total duration of requested music"] = f"{str(hours).zfill(2)}:{str(mins).zfill(2)}:{str(seconds).zfill(2)}"
+
+    # Most requested song
+    if len(data) > 0:
+        most_popular_id = max(data, key= lambda song: data[song])
+        most_popular_title = song_data[most_popular_id]["title"]
+        request_count = data[most_popular_id]
+        msg = f"{most_popular_title} with {request_count} requests"
+    else:
+        msg = "N/A"
+    user_stats["Most requested song"] = msg
+
+    return user_stats
+
+@bot.command(name='stats', help='Retrieves usage statistics for the bot and its users.  Will print global statistics if given no arguments or will give user arguments if a user is mentioned as an argument.')
+async def stats(ctx, *args):
+    if len(args) == 0:
+        global_stats = calculate_global_stats()
+        msg = "Global statistics:\n" + "\n".join([f"{name}: {value}" for name, value in global_stats.items()])
+    else:
+        user = args[0]
+        user = user[:2] + "!" + user[2:]
+        if user in user_data:
+            user_stats = calculate_user_stats(user)
+            msg = f"Statistics for user {user}:\n" + "\n".join([f"{name}: {value}" for name, value in user_stats.items()])
+        else:
+            msg = f"User {user} not found."
     await ctx.send(msg)
 
 if __name__ == "__main__":
-    # Register hooks to write the stats dictionaries to disk before exiting
-    atexit.register(lambda *args: write_stats(USER_STATS_FILE, user_stats))
-    atexit.register(lambda *args: write_stats(SONG_STATS_FILE, song_stats))
+    # Register hooks to write the data dictionaries to disk before exiting
+    atexit.register(lambda *args: write_json_file(USER_FILE, user_data))
+    atexit.register(lambda *args: write_json_file(SONG_FILE, song_data))
 
     # Run the bot
     bot.run(DISCORD_TOKEN)
