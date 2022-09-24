0be87a2340c9265b7f74f593e889d133f57d6688
Alex McQuilkin <alexmcq99@gatech.edu>
2022-09-24 12:17:44 -0700

    Made a setup.py script to download ffmpeg.exe

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+import urllib.request
+import zipfile
+
+# Download zip file for ffmpeg
+DOWNLOAD_URL = "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip"
+zip_file, _ = urllib.request.urlretrieve(DOWNLOAD_URL)
+
+# Extract ffmpeg.exe and place in root directory
+OLD_PATH = "ffmpeg-master-latest-win64-gpl/bin/ffmpeg.exe"
+with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+    zip_ref.extract(OLD_PATH)
+
+# Clean up files
+NEW_PATH = "ffmpeg.exe"
+os.rename(OLD_PATH, NEW_PATH)
+shutil.rmtree(OLD_PATH.split("/")[0])
+os.remove(zip_file)
